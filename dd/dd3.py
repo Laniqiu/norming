579103a3954b1b500dfe8063455d0e91ad441d28
laniqiu <laniqiu@hotmail.com>
2022-12-19 20:17:03 +0800

    create main entrance

--- dd/dd3.py
+++ dd/dd3.py
@@ -7,7 +7,7 @@ import logging
 from collections import OrderedDict
 
 import sys
-sys.path.insert(0, "/home/MyDrive/pies/dd")  # colab: import
+sys.path.insert(0, "/content/laniqiu/MyDrive/dough")  # colab: import
 from dd2 import load_sents_parts
 
 logging.basicConfig(level=logging.INFO)
@@ -37,70 +37,41 @@ def stanza_dp(sents, lang, processors='tokenize,pos,lemma,depparse'):
         #     for sent in doc.sentences for word in sent.words], sep='\n')
     return res
 
-    def main(root, out_dir):
-        import os
-        from glob import glob
-        import json
-        import codecs
 
-        if not os.path.exists(out_dir):
-            os.mkdir(out_dir)
-
-        files = glob(os.path.join(root, "*.txt"))
-        files.sort()
-        for f in files:
-            lang = "zh" if "simp" in f else "zh-hant"
-            logging.info("language:{}".format(lang))
-
-            sents, all_ = load_sents_parts(f)
-
-            wsents = OrderedDict()  # full sents
-            for sid, isen in sents.items():
-                wid, sent = zip(*isen)
-                wsents[sid] = "".join(sent)
-            res = stanza_dp(wsents, lang)
-
-            headline = ["sent_id", "word_id", "text", "lemma", "upos", "xpos", "head", "deprel"]
-            res.insert(0, "\t".join(headline))
-
-            fout = f.replace(root, out_dir)
-            with codecs.open(fout, "w") as fw:
-                fw.writelines(res)
-
-
-if __name__ == "__main__":
+def main(root, out_dir):
     import os
     from glob import glob
     import json
     import codecs
 
-    root = "/home/MyDrive/annotator_avg/"
-    out_dir = "/home/MyDrive/parsed_res/"
+    if not os.path.exists(out_dir):
+        os.mkdir(out_dir)
+
+    files = glob(os.path.join(root, "*.txt"))
+    files.sort()
+    for f in files:
+        lang = "zh" if "simp" in f else "zh-hant"
+        logging.info("language:{}".format(lang))
+
+        sents, all_ = load_sents_parts(f)
+
+        wsents = OrderedDict()  # full sents
+        for sid, isen in sents.items():
+            wid, sent = zip(*isen)
+            wsents[sid] = "".join(sent)
+        res = stanza_dp(wsents, lang)
+
+        headline = ["sent_id", "word_id", "text", "lemma", "upos", "xpos", "head", "deprel"]
+        res.insert(0, "\t".join(headline))
+
+        fout = f.replace(root, out_dir)
+        with codecs.open(fout, "w") as fw:
+            fw.writelines(res)
+
+
+if __name__ == "__main__":
+    root = "/Users/laniqiu/My Drive"
+    out_dir = "/Users/laniqiu/My Drive/parsed_res"
 
     main(root, out_dir)
 
-    # root = "/home/MyDrive/annotator_avg/"
-    # out_dir = "/home/MyDrive/parsed_res/"
-    # if not os.path.exists(out_dir):
-    #     os.mkdir(out_dir)
-    #
-    # files = glob(os.path.join(root, "*.txt"))
-    # files.sort()
-    # for f in files:
-    #     lang = "zh" if "simp" in f else "zh-hant"
-    #     logging.info("language:{}".format(lang))
-    #
-    #     sents, all_ = load_sents_parts(f)
-    #
-    #     wsents = OrderedDict()  # full sents
-    #     for sid, isen in sents.items():
-    #         wid, sent = zip(*isen)
-    #         wsents[sid] = "".join(sent)
-    #     res = stanza_dp(wsents, lang)
-    #
-    #     headline = ["sent_id", "word_id", "text", "lemma", "upos", "xpos", "head", "deprel"]
-    #     res.insert(0, "\t".join(headline))
-    #
-    #     fout = f.replace(root, out_dir)
-    #     with codecs.open(fout, "w") as fw:
-    #         fw.writelines(res)
