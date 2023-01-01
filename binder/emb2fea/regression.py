9a4512c9289c37b10168f9c29a48610e0162f821
Lani's Little Bakery <laniqiu@hotmail.com>
2023-01-01 23:44:46 +0800

    to colab

--- binder/emb2fea/regression.py
+++ binder/emb2fea/regression.py
@@ -5,26 +5,96 @@ steps:
     train regression models
     evaluation
 """
-
-try:
-    from google.colab import drive
-    # drive.mount('/content/drive')
-    _root = "/content/drive/MyDrive/"
-except:
-    _root = "/Users/laniqiu/My Drive/"
-
-import logging
-
+from sklearn.ensemble import RandomForestRegressor
+from sklearn.linear_model import LinearRegression, Ridge, Lasso
+from sklearn.neural_network import MLPRegressor
+from sklearn.model_selection import LeaveOneOut
+from scipy.stats import spearmanr
+from bokeh.themes import default
+import numpy as np
 
 from utils import *
+from common import logging, get_root
 
 
-LOG_FORMAT = "%(asctime)s - %(levelname)s - %(message)s"
-logging.basicConfig(level=logging.INFO, format=LOG_FORMAT)
+def main(fpth, efolder):
+    """
+    @param fpth: data path
+    @param efolder: folder of embeddings
+    @return:
+    """
+    logging.info("load data from {} ...".format(fpth.name))
+    _data = load_data(fpth)
 
+    epths = efolder.glob("*")
+    for epth in epths:
+        if epth.name.startswith("."):  # in case hidden files exist
+            continue
+        # todo
+        if epth.name not in ["cc.zh.300.vec"]:
+            continue
+        logging.info("load embeddings from {} ...".format(epth.name))
+        vectors, dim = load_embeddings(epth, _data)
 
+        X, Y, words = assign_emb_dataset(_data, _data, vectors, dim)
+
+        loo = LeaveOneOut()
+        logging.info("number of splits {}".format(loo.get_n_splits(X)))
+        Spear, Ms, Rm = [], [], []
+        Y_output, Y_gold = [], []
+
+        for i, (train_index, test_index) in enumerate(loo.split(X)):
+
+            X_train, X_test = X[train_index], X[test_index]
+            Y_train, Y_test = Y[train_index], Y[test_index]
+
+            # different regressors
+            model = LinearRegression().fit(X_train, Y_train)
+            # model = Ridge().fit(X_train, Y_train)
+            # model = RandomForestRegressor(n_estimators=10).fit(X_train, Y_train)
+            # model = MLPRegressor(hidden_layer_sizes=(50, 10), activation='identity', solver='adam', early_stopping=True,
+            #                      max_iter=1000).fit(X_train, Y_train)
+            # model = Lasso(alpha=0.1).fit(X_train, Y_train)
+
+            Y_pred = model.predict(X_test)
+            mse, rmse = return_MSE_by_Feature(Y_test, Y_pred)  # rmse is the sqrt of mse
+
+            for i in range(Y_pred.shape[0]):
+                Y_output.append(Y_pred[i])
+                Y_gold.append(Y_test[i])
+
+            Ms.append(mse)
+            Rm.append(rmse)
+
+            # 	print ( "Global evaluation score for the mapper: " + map )
+        Ms_means = np.mean(Ms, axis=0)
+        Rm_means = np.mean(Rm, axis=0)
+
+        Sp_means = return_Spearman_simple(Y_gold, Y_output)
+
+        print(Ms_means)
+        print(Rm_means)
+        print(Sp_means)
+
+        # evaluation
+        Y_output, Y_gold = np.array(Y_output), np.array(Y_gold)
+        rows, cols = Y_gold.shape
+
+        # result presenting
+        for i in range(0, cols):
+            sense_gold = Y_gold[:, i]
+            sense_output = Y_output[:, i]
+            score = spearmanr(sense_gold, sense_output)
+        breakpoint()
 
 
 if __name__ == '__main__':
+    _root = get_root()
+
     from pathlib import Path
-    _path = Path(_root)
+    _path = Path(_root).joinpath("dough")
+    fpth = _path.joinpath("Copy of meanRating_July1.xlsx")
+    # epth = _path.joinpath("norming/embeddings/cc.zh.300.vec")
+    efolder = _path.joinpath("norming/embeddings/")
+    main(fpth, efolder)
+
--- binder/emb2fea/utils.py
+++ binder/emb2fea/utils.py
@@ -1,15 +1,14 @@
 """
 utils
 """
-import pandas as pd
+import heapq
 import numpy as np
-
-try:
-    from google.colab import drive
-    # drive.mount('/content/drive')
-    _root = "/content/drive/MyDrive/"
-except:
-    _root = "/Users/laniqiu/My Drive/"
+import pandas as pd
+from scipy.spatial.distance import cosine
+from scipy.stats import spearmanr
+from sklearn.metrics import mean_squared_error
+from sklearn.metrics import explained_variance_score
+from sklearn.metrics import r2_score
 
 
 def load_data(fname, chinese=True):
@@ -40,7 +39,7 @@ def load_data(fname, chinese=True):
 def load_embeddings(fname, ds_words):
     """
     @param fname: path of word embeddings
-    @param ds_words:
+    @param ds_words: target words
     @return:
     """
     emb = {}
@@ -56,7 +55,6 @@ def load_embeddings(fname, ds_words):
                     dims = len(line) - 1
 
             word = line[0]
-
             if word not in ds_words:
                 continue
 
@@ -74,7 +72,7 @@ def load_embeddings(fname, ds_words):
             if word in emb.keys():
                 continue
 
-            emb[word] = len(matrix)
+            emb[word] = len(matrix)  # as indices
             matrix.append(arrays)
 
     print(len(matrix))
@@ -83,6 +81,97 @@ def load_embeddings(fname, ds_words):
     return emb, dims
 
 
-if __name__ == '__main__':
-    from pathlib import Path
-    _path = Path(_root)
+def get_vec(word, embs, PRINT_WORD=False):
+    """
+
+    @param word:
+    @param embs:
+    @param PRINT_WORD:
+    @return:
+    """
+    if word in embs.keys():
+        try:
+            return embs['_matrix_'][embs[word], :]
+        except:
+            if word != "_matrix_":
+                print('{} should have been there but something went wrong when loading it!'.format(word))
+            return []
+    else:
+        if PRINT_WORD:
+            print('{} not in the dataset.'.format(word))
+
+        # return np.zeros(embs['_matrix_'][0].shape[0])
+        return []
+
+
+def assign_emb_dataset(ds, ds_words, embs, dim, norm_dim=68):
+    """
+    @param ds: dataset
+    @param ds_words: words
+    @param embs:
+    @param dim:
+    @param norm_dim: size of attributes/ features
+    @return:
+        X: dataset for training and validation
+        Y: labels
+        words: words
+    """
+
+    words, X, Y = [], [], []
+    for i, word in enumerate(ds_words):
+        if word not in list(embs.keys()):
+            print('Word {} does not appear in embs'.format(word))
+            continue
+        words.append(word)
+
+        # the second condition needs to be changed for predictions on Dutch norms
+        if word in ['domains', '_matrix_']:
+            continue
+
+        vec = get_vec(word, embs)
+        norm = get_vec(word, ds)
+
+
+
+        if len(vec) != dim or len(norm) != norm_dim:
+            continue
+
+        X.append(vec)
+        Y.append(norm)
+    X, Y = np.array(X), np.array(Y)
+    """print (X)
+    print (Y)
+    print (type(X))
+    print (type(Y))"""
+
+    return X, Y, words
+
+
+def return_MSE_by_Feature(Y_test, Y_pred):
+    mse = mean_squared_error(Y_test, Y_pred, multioutput='raw_values')
+    rmse = np.sqrt(mse)
+
+    return mse, rmse
+
+
+def return_Spearman_simple(Y_test, Y_pred):
+    spear_var = []
+    if np.asarray(Y_test).shape != np.asarray(Y_pred).shape:
+        assert False, "The size of the prediction array Y and of the test array Y are different."
+
+    # Y_test, Y_pred = np.array(Y_test), np.array(Y_pred)
+    # print (Y_test)
+    # print (Y_pred)
+
+    Y_pred = np.array(Y_pred)
+    Y_test = np.array(Y_test)
+    for i in range(len(Y_test[0])):
+        var = spearmanr(Y_test[:, i], Y_pred[:, i])[0]
+        spear_var.append(var)
+
+    # print (spear_var)
+    return np.array(spear_var, dtype=float)
+
+
+
+
