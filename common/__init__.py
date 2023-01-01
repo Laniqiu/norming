8a0b25a74235c5b456a7fc61a22c3d17aa9c136e
Lani's Little Bakery <laniqiu@hotmail.com>
2023-01-01 19:47:31 +0800

    add logger

---
+++ common/__init__.py
@@ -0,0 +1,9 @@
+from .logger import MyLogger
+logging = MyLogger()
+
+def get_root():
+    try:
+        from google.colab import drive
+        return "/content/drive/MyDrive/"
+    except:
+        return "/Users/laniqiu/My Drive"
